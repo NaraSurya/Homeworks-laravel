f2f01dfb72016c0d91274d2b011ca9154b64e873
Nara Surya <ibagungnsd13@gmail.com>
2018-11-08 16:17:14 +0800

    deploy heroku

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-gcp-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'gcp_23cea052fc2fda415414'),
+            'username' => env('DB_USERNAME', 'bf7d3de5681ece'),
+            'password' => env('DB_PASSWORD', '5a858519'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
